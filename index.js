6e71e1a84a0a35638c23c5f7ebeb559a8f240e3b
a1000rhan <1000rhan@gmail.com>
2021-12-25 11:34:56 +0300

    no dublication

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // Premade function that will fill the button with its number.
 // First button top left is called 1, last button bottom right is 9
+let turn=0;
+let cond;
 function fillButton(index, text) {
   // This function fills
+  
+ 
   document.getElementById(index).innerHTML = text;
+  
+  
 }
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // Delete this code once you are done testing
 
-let timer=0;
 
 
 /**
@@ -18,27 +23,37 @@ let timer=0;
  * Add your code here, since this is going to be your main function
  * That interacts with the UI
  */
+
 function clickButton(index) {
+  
+  if(turn >=0 && turn <=8){
+    console.log(cond);
+    if(cond != index){
+    fillButton(index,checkPlayer()); 
+   cond =index;
+   return turn++;
+    }
+  }
  
   
-  fillButton(index,checkPlayer());
+    
+    
+  } 
   
-  timer++;
-
-
-}
-
-
   
-
-
-// in this function you should check if the player is X or O
-function checkPlayer() {
-if(timer%2==0){
-  return "X";
-}else{
-  return "O";
-}
+  
+  // in this function you should check if the player is X or O
+  function checkPlayer() {
+    
+    
+    
+    if(turn%2==0){
+      
+      return "X";
+    }else{
+      return "O";
+    }
+    
 
 }
 /**
@@ -47,6 +62,9 @@ if(timer%2==0){
  * who is winning and returns the winner
  */
 // function checkWinner
+function checkWinner(){
+  
+}
 
 // function restart game
 
